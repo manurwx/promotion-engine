19806baa773f1bc6a91b3ecb57e7b10e92318a91
Manu Amico <manu123@pm.me>
2022-07-02 17:57:33 +0200

    Full dockerization of project

--- SQL/product.sql
+++ SQL/product.sql
@@ -1,3 +1,3 @@
-INSERT INTO `product` (`id`, `price`) VALUES
-(1, 100),
-(2, 200);
+INSERT INTO `product` (`id`,`price`) VALUE
+(1,100),
+(2,200);
--- SQL/promotion.sql
+++ SQL/promotion.sql
@@ -1,3 +1,4 @@
-INSERT INTO `promotion` (`id`, `name`, `type`, `adjustment`, `criteria`) VALUES
-(1, 'Black Friday half price sale', 'date_range_multiplier', 0.5, '{"to": "2021-11-28", "from": "2022-11-25"}'),
-(2, 'Voucer OU182', 'fixed_price_voucher', 100, '{"code": "OU812"}');
+INSERT INTO `promotion` (`id`,`name`,`type`,`adjustment`,`criteria`) VALUES
+(1,'Black Friday half price sale','date_range_multiplier',0.5,'{\"to\": \"2022-11-28\", \"from\": \"2022-11-25\"}'),
+(2,'Voucer OU182','fixed_price_voucher',100,'{\"code\": \"OU812\"}'),
+(3,'Buy one get one free','even_items_multiplier',0.5,'{\"minimum_quantity\": 2}');
